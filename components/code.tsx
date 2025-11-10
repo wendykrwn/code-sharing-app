b5726afe0b2356159cab18d10712a7e76f1b33af
wendykrwn <wendykrwn@gmail.com>
2025-11-10 03:34:54 +0100

    add monaco editor

--- components/code.tsx
+++ components/code.tsx
@@ -1,7 +1,13 @@
+"use client"
+import { defaultHtmlCode } from "@/utils/codeExample"
+import { Editor } from "@monaco-editor/react"
+
 const Code = () => {
     return (
-        <div className="bg-white ">
-            Mon Code
+        <div className="bg-white h-[80vh] w-5xl m-auto rounded-2xl py-5">
+            <div className="h-[90%]">
+                <Editor defaultLanguage="html" defaultValue={defaultHtmlCode} />
+            </div>
         </div>
     )
 }
